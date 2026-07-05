79cee14b4490b4ae46754db2ed5b3593a25cd1e3
ElieOko <elieoko100@gmail.com>
2026-07-05 10:17:36 +0100

    feat:initialisation du viewmodel et refactorisation

--- app/src/main/java/elieoko/app/mcoresystem/MainActivity.kt
+++ app/src/main/java/elieoko/app/mcoresystem/MainActivity.kt
@@ -3,21 +3,39 @@ package elieoko.app.mcoresystem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.activity.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import elieoko.app.mcoresystem.presentation.ui.theme.MCoreSystemTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import elieoko.app.mcoresystem.domain.viewmodel.config.*
+import elieoko.app.mcoresystem.domain.viewmodel.room.*
+import elieoko.app.mcoresystem.presentation.ui.theme.*
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+    private val userViewModel: UserViewModel by viewModels { UserViewModelFactory((application as MCoreApplication).userRepository) }
+    private val currencyViewModel: CurrencyViewModel by viewModels { CurrencyViewModelFactory((application as MCoreApplication).currencyRepository) }
+    private val paymentMethodViewModel: PaymentMethodViewModel by viewModels { PaymentMethodViewModelFactory((application as MCoreApplication).paymentMethodRepository) }
+    private val operationViewModel: OperationViewModel by viewModels { OperationViewModelFactory((application as MCoreApplication).operationRepository) }
+    private val organismViewModel: OrganismViewModel by viewModels { OrganismViewModelFactory((application as MCoreApplication).organismRepository) }
+    private val categoryViewModel: CategoryViewModel by viewModels { CategoryViewModelFactory((application as MCoreApplication).categoryRepository) }
+    private val typeCategoryViewModel: TypeCategoryViewModel by viewModels { TypeCategoryViewModelFactory((application as MCoreApplication).typeCategoryRepository) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val applicationViewModel = viewModel<ApplicationViewModel>()
+            applicationViewModel.room.currency = currencyViewModel
+            applicationViewModel.room.user = userViewModel
+            applicationViewModel.room.paymentMethod = paymentMethodViewModel
+            applicationViewModel.room.operation = operationViewModel
+            applicationViewModel.room.category = categoryViewModel
+            applicationViewModel.room.typeCategory = typeCategoryViewModel
+            applicationViewModel.room.organism = organismViewModel
             MCoreSystemTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
